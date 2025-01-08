bee0feeeff699836fd983b66f750a77cb8230ed5
Nikhil <nikhilbalamurugan@gmail.com>
2025-01-08 20:24:53 +0530

    fixed the issues

---
+++ examples/degreeCal.cpp
@@ -0,0 +1,28 @@
+#include "MixedGraph.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+    Appledore::MixedGraphMatrix<std::string> graph;
+
+    graph.addVertex("A");
+    graph.addVertex("B");
+    graph.addVertex("C");
+
+    graph.addEdge("A", "B", true);  // Directed edge A -> B
+    graph.addEdge("B", "C");       // Undirected edge B <-> C
+
+    std::cout << "Vertices: ";
+    for (const auto &vertex : graph.getVertices())
+    {
+        std::cout << vertex << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "In-degree of B: " << graph.indegree("B") << std::endl;   // Should be 1
+    std::cout << "Out-degree of B: " << graph.outdegree("B") << std::endl; // Should be 2
+    std::cout << "Total degree of B: " << graph.totalDegree("B") << std::endl; // Should be 3
+
+    return 0;
+}
