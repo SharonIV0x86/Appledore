60641a1b581b1404d8c4bd5be7dcbb253bae033f
Swayam Prakash Kar <swayam123code@gmail.com>
2025-01-11 00:22:51 +0530

    Add example for vertex removal in MixedGraph

--- examples/vertexRemoval.cpp
+++ examples/vertexRemoval.cpp
@@ -1,80 +1,115 @@
 #include <iostream>
-#include "../include/GraphMatrix.h" 
+#include "../include/MixedGraph.h"  
+
+using namespace Appledore;
+
+
+class City : public GraphVertex
+{
+public:
+    std::string name;
+
+    City(const std::string &name) : name(name) {}
+
+    // Overload to format output, optional
+    friend std::ostream &operator<<(std::ostream &os, const City &city)
+    {
+        os << city.name;
+        return os;
+    }
+};
+
+
+class Road
+{
+public:
+    int distance;
+
+    Road(int dist) : distance(dist) {}
+
+    // Overload to format output, optional
+    friend std::ostream &operator<<(std::ostream &os, const Road &road)
+    {
+        os << road.distance << " miles";
+        return os;
+    }
+};
+
 int main()
 {
-    // Here, we assume you are using:
-    // GraphMatrix<VertexType, EdgeType, Direction>
-    // Let's pick: GraphMatrix<std::string, int, UndirectedG> for this demo.
-    Appledore::GraphMatrix<std::string, int, Appledore::UndirectedG> graph;
-
-   
-    graph.addVertex("A", "B", "C", "D");
-
-    // 2. Add edges
-    // (For an undirected graph, an edge from A to B also implies an edge from B to A.)
-    graph.addEdge("A", "B", 1);
-    graph.addEdge("B", "C", 2);
-    graph.addEdge("C", "D", 3);
-    graph.addEdge("A", "D", 4);
-
-    // 3. Display the initial vertices
-    std::cout << "Initial Vertices:\n";
-    for (const auto &v : graph.getVertices())
-    {
-        std::cout << v << " ";
-    }
-    std::cout << "\n\n";
-
-    // 4. Display the initial edges
-    std::cout << "Initial Edges (Undirected):\n";
-    auto edges = graph.getAllEdges();
-    for (const auto &edge : edges)
-    {
-        auto &[src, dest, val] = edge;
-        std::cout << src << " --(" << val << ")--> " << dest << "\n";
-    }
-    std::cout << "\n";
-
-    // 5. Remove a vertex (say "C")
-    std::cout << "Removing vertex: C\n\n";
     try
     {
-        graph.removeVertex("C");
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Error: " << e.what() << "\n";
-    }
+        MixedGraphMatrix<City, int> cityGraph;
 
-    // 6. Display the updated vertices
-    std::cout << "Updated Vertices:\n";
-    for (const auto &v : graph.getVertices())
-    {
-        std::cout << v << " ";
-    }
-    std::cout << "\n\n";
+        // Define some cities
+        City nyc("New York");
+        City la("Los Angeles");
+        City chi("Chicago");
+        City sf("San Francisco");
 
-    // 7. Display the updated edges
-    std::cout << "Updated Edges (Undirected):\n";
-    edges = graph.getAllEdges();
-    for (const auto &edge : edges)
-    {
-        auto &[src, dest, val] = edge;
-        std::cout << src << " --(" << val << ")--> " << dest << "\n";
-    }
-    std::cout << "\n";
+        // Add vertices
+        cityGraph.addVertex(nyc);
+        cityGraph.addVertex(la);
+        cityGraph.addVertex(chi);
+        cityGraph.addVertex(sf);
 
-    // 8. Try checking edges involving removed vertex "C"
-    std::cout << "Attempting to check edge from removed vertex C:\n";
-    try
-    {
-        // Should throw an exception since "C" no longer exists
-        bool edgeExists = graph.hasEdge("C", "D");
-        std::cout << "Unexpected: 'C' to 'D' edge found? " << (edgeExists ? "Yes" : "No") << "\n";
+        // Add edges (if using MixedGraphMatrix with int as EdgeType, pass integers)
+        // For demonstration, assume all roads are undirected and set distances
+        cityGraph.addEdge(nyc, la, 2800 /* miles */, false);
+        cityGraph.addEdge(nyc, chi, 790, false);
+        cityGraph.addEdge(la, sf, 383, false);
+        cityGraph.addEdge(chi, sf, 2130, false);
+
+        // Display current vertices
+        std::cout << "Cities in the graph:\n";
+        for (auto &vertex : cityGraph.getVertices())
+        {
+            std::cout << "- " << vertex.name << "\n";
+        }
+
+        // Display current edges (if using MixedGraphMatrix<int>)
+        // We'll just show they exist by checking them
+        std::cout << "\nRoad distances in the graph:\n";
+        for (auto &src : cityGraph.getVertices())
+        {
+            for (auto &dest : cityGraph.getVertices())
+            {
+                if (cityGraph.hasEdge(src, dest))
+                {
+                    std::cout << src.name << " <-> " << dest.name
+                              << " : " << cityGraph.getEdgeValue(src, dest) << " miles\n";
+                }
+            }
+        }
+
+        // Remove a vertex
+        std::cout << "\nRemoving the city: " << chi.name << "...\n";
+        cityGraph.removeVertex(chi);
+
+        // Display vertices after removal
+        std::cout << "\nCities in the graph after removal:\n";
+        for (auto &vertex : cityGraph.getVertices())
+        {
+            std::cout << "- " << vertex.name << "\n";
+        }
+
+        // Display edges after removal
+        std::cout << "\nRoad distances in the graph after removing " << chi.name << ":\n";
+        for (auto &src : cityGraph.getVertices())
+        {
+            for (auto &dest : cityGraph.getVertices())
+            {
+                if (cityGraph.hasEdge(src, dest))
+                {
+                    std::cout << src.name << " <-> " << dest.name
+                              << " : " << cityGraph.getEdgeValue(src, dest) << " miles\n";
+                }
+            }
+        }
     }
-    catch (const std::exception &e)
+    catch (const std::exception &ex)
     {
-        std::cerr << "Expected error: " << e.what() << "\n";
+        std::cerr << "Error: " << ex.what() << std::endl;
     }
 
     return 0;
