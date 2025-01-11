47984f932488811756d207234b618406829b6424
ash01825 <niteshtulshyan998@gmail.com>
2025-01-12 03:45:58 +0530

    made changes to connectedGraph.cpp to include discussed and fixed removeVertex

--- examples/connectedGraph.cpp
+++ examples/connectedGraph.cpp
@@ -20,7 +20,7 @@ class Weight
 {
 public:
     int value;
-    Weight(int v = 1) : value(v) {}  // Default constructor with value 1
+    Weight(int v = 1) : value(v) {}
 
     friend std::ostream& operator<<(std::ostream& os, const Weight& w) {
         os << w.value;
@@ -35,36 +35,51 @@ void printGraphConnectivity(const auto& graph, const std::string& description) {
 }
 
 int main() {
-    GraphMatrix<TestVertex, Weight, UndirectedG> connectedGraph;
+    GraphMatrix<TestVertex, Weight, UndirectedG> graph1;
 
-    TestVertex v1("A"), v2("B"), v3("C"), v4("D");
-    connectedGraph.addVertex(v1, v2, v3, v4);
+    TestVertex v1("A"), v2("B"), v3("C"), v4("D"), v5("E");
+    graph1.addVertex(v1, v2, v3, v4, v5);
 
-    connectedGraph.addEdge(v1, v2, Weight());
-    connectedGraph.addEdge(v2, v3, Weight());
-    connectedGraph.addEdge(v3, v4, Weight());
+    graph1.addEdge(v1, v2, Weight(5));
+    graph1.addEdge(v2, v3, Weight(3));
+    graph1.addEdge(v3, v4, Weight(4));
+    graph1.addEdge(v4, v5, Weight(2));
+    graph1.addEdge(v5, v1, Weight(6));
 
-    printGraphConnectivity(connectedGraph, "Graph 1");
+    printGraphConnectivity(graph1, "Graph 1: Connected");
 
-    GraphMatrix<TestVertex, Weight, UndirectedG> disconnectedGraph;
+    graph1.removeEdge(v1, v5);
+    printGraphConnectivity(graph1, "Graph 1: After removing edge A-E");
 
-    TestVertex v5("E"), v6("F"), v7("G"), v8("H");
-    disconnectedGraph.addVertex(v5, v6, v7, v8);
+    graph1.removeVertex(v5);
+    printGraphConnectivity(graph1, "Graph 1: After removing vertex E");
 
-    disconnectedGraph.addEdge(v5, v6, Weight());
-    disconnectedGraph.addEdge(v7, v8, Weight());
+    GraphMatrix<TestVertex, Weight, UndirectedG> graph2;
 
-    printGraphConnectivity(disconnectedGraph, "Graph 2");
+    TestVertex v6("F"), v7("G"), v8("H"), v9("I"), v10("J");
+    graph2.addVertex(v6, v7, v8, v9, v10);
 
-    GraphMatrix<TestVertex, Weight, DirectedG> directedGraph;
+    graph2.addEdge(v6, v7, Weight(1));
+    graph2.addEdge(v8, v9, Weight(2));
 
-    TestVertex v9("I"), v10("J"), v11("K");
-    directedGraph.addVertex(v9, v10, v11);
+    printGraphConnectivity(graph2, "Graph 2: Disconnected");
 
-    directedGraph.addEdge(v9, v10, Weight());
-    directedGraph.addEdge(v10, v11, Weight());
+    graph2.addEdge(v7, v8, Weight(3));
+    printGraphConnectivity(graph2, "Graph 2: After connecting components");
 
-    printGraphConnectivity(directedGraph, "Graph 3");
+
+    GraphMatrix<TestVertex, Weight, DirectedG> graph3;
+
+    TestVertex v11("K"), v12("L"), v13("M"), v14("N");
+    graph3.addVertex(v11, v12, v13, v14);
+
+    graph3.addEdge(v11, v12, Weight(1));
+    graph3.addEdge(v12, v13, Weight(2));
+
+    printGraphConnectivity(graph3, "Graph 3: Directed");
+
+    graph3.addEdge(v13, v11, Weight(3));
+    printGraphConnectivity(graph3, "Graph 3: After adding reverse edge M-K");
 
     return 0;
 }
